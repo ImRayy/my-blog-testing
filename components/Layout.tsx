0ab51c3e2481164ed4cbfd35eb0ae9929158458f
ImRayy <souradeep20roy01@gmail.com>
2023-03-31 11:14:42 +0530

    Replaced page transition animation
    Sliding animation causing lag on few devices

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,10 +10,11 @@ interface LayoutProps {
 }
 
 const variants = {
-  initial: { opacity: 0, x: -200, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 0, y: -100 },
+  initial: { opacity: 0 },
+  enter: { opacity: 1 },
+  exit: { opacity: 0 },
 };
+
 export default function Layout({ children }: LayoutProps) {
   const [toggle, setToggle] = useState(false);
   const router = useRouter();
