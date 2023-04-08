ea44ebb8013422ecb60eaa0b1457d9dfe9e0c099
ImRayy <souradeep20roy01@gmail.com>
2023-04-09 02:11:29 +0530

    Updated sanity/Client

--- components/sanity/Client.ts
+++ components/sanity/Client.ts
@@ -3,5 +3,6 @@ import { createClient } from "next-sanity";
 export const ConfiguredSanityClient = createClient({
   projectId: process.env.PROJECT_ID,
   dataset: "production",
+  apiVersion: "2023-04-04",
   useCdn: true,
 });
