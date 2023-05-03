a0f582e6fb48706d8de694df1054a47569342bf4
ImRayy <souradeep20roy01@gmail.com>
2023-05-04 00:15:00 +0530

    Added react-top-loading-bar

--- components/sanity/Client.ts
+++ components/sanity/Client.ts
@@ -1,8 +1,9 @@
 import { createClient } from "next-sanity";
 
 export const ConfiguredSanityClient = createClient({
-  projectId: process.env.PROJECT_ID,
-  dataset: "production",
-  apiVersion: "2023-04-04",
-  useCdn: true,
+    projectId: process.env.PROJECT_ID,
+    token: process.env.TOKEN,
+    dataset: "production",
+    apiVersion: "2023-04-04",
+    useCdn: true,
 });
--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    THEME_JSON: process.env.THEME_JSON,
-    PROJECT_ID: process.env.PROJECT_ID,
-  },
-};
+    reactStrictMode: true,
+    env: {
+        THEME_JSON: process.env.THEME_JSON,
+        PROJECT_ID: process.env.PROJECT_ID,
+        TOKEN: process.env.TOKEN,
+    },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
